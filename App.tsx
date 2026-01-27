2675971dbc1667a52efe14a483e5ba81c2c5d1fc
Ayman_Chabchoub <chabchoubayman5@gmail.com>
2026-01-27 18:46:09 +0100

    message

--- App.tsx
+++ App.tsx
@@ -1,45 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppTemplate from './src/templates/AppTemplate';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import LoginScreen from './src/screens/Login';
+import HomeScreen from './src/screens/Home';
+import Tabs from './src/navigations/Tabs';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ProductDetailScreen from './src/screens/ProductDetail';
+import RegisterScreen from './src/screens/Register';
+import { useState } from 'react';
+import LoadingScreen from './src/screens/Loading';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    // Affiche le loading screen avant de montrer l'app
+    return <LoadingScreen onLoaded={() => setLoading(false)} />;
+  }
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <NavigationContainer>
+        <AppTemplate>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Tabs" component={Tabs} />
+            <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+          </Stack.Navigator>
+        </AppTemplate>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
